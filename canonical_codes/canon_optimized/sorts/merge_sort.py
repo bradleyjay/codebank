d075328481b789168cb51d70d502e238d8a8ab54
Bradley Shields <bradleythered@Bradleys-MacBook-Pro.local>
2018-09-01 21:37:17 -0400

    Edits to mergesort, bin_search notes.

--- canonical_codes/canon_optimized/sorts/merge_sort.py
+++ canonical_codes/canon_optimized/sorts/merge_sort.py
@@ -3,52 +3,51 @@
 # uses 2N space in calc, but 2N = N for large N (this applies for space..?)
 
 ''' 
-merge sort:
 
-1) base case
-2) Save recursive call on L, R
-3) Add smaller value from each side to temp list using pos indicies, Result
-4) Add remaining
-5) Return result
+Recursive splits (logN), merge back together (N) -> (N log N)
+
+1) Base case: len(x) < 2: return x
+2) Set mid, call recursive on L, R
+3) Add smaller val from each side to RESULT with position indicies
+4) Add remaining elements
+5) return RESULT
 
 '''
 
-def msort3(x):
+def merge_sort(x):
 
-    # 1) base case (array size)
-    
-    if len(x) < 2:
-        return x
-    
-    # 2) Call recursives on L, R
+    # Step 1: Base Case
+    if len(x) < 2: return x
 
-    mid = len(x) // 2
-    
-    left_half = msort3(x[:mid])
-    right_half = msort3(x[mid:])
+    # Step 2: Set mid, call recursives on L, R
 
-    #3) Add smaller value from each side to RESULT using pos indicies
+    mid = len(x)//2
 
-    result = []
+    L = merge_sort(x[:mid])
+    R = merge_sort(x[mid:])
+
+    # Step 3: Add smaller val from each side to RESULT using position indicies
+
     i = 0
     j = 0
+    result = []
 
-    while i < len(left_half) and j < len(right_half):
-        
-        if left_half[i] > right_half[j]:
-            result.append(right_half[j])
-            j += 1
-        else:
-            result.append(left_half[i])
+    while i < len(L) and j < len(R):
+
+        if L[i] < R[j]:
+            result.append(L[i])
             i += 1
 
-    # 4) Add remaining elements
+        else:
+            result.append(R[j])
+            j += 1
 
-    result += left_half[i:]
-    result += right_half[j:]
+    # Step 4: Add remaining elements
+    result += L[i:]
+    result += R[j:]
 
-    # 5) return RESULT
+    # Step 5: Return RESULT
     return result
 
 a = [5,2,3,77,22,55,33]
-print(msort3(a))
+print(merge_sort(a))
