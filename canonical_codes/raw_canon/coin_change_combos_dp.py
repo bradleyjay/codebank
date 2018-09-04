3c0e63a7750603e6909e0176de4daf255f85473b
Bradley Shields <bradleythered@Bradleys-MacBook-Pro.local>
2018-09-03 21:53:08 -0400

    Commented up coin_change_combos_dp.py

--- canonical_codes/raw_canon/coin_change_combos_dp.py
+++ canonical_codes/raw_canon/coin_change_combos_dp.py
@@ -2,35 +2,48 @@
 
 def coin_change(amount, coins):
 
-    # Step 1: Make array: 0 -> in target value columns, one row per "allowed" coin
+    # Step 1: Initialize matrix 'combos'
+    #         One column in 1's up to target value
+    #         One row per coin "allowed yet" 
+    #         0's everywhere, except 0 value column gets 1's
+
     combos = [[ 0 for j in range(0,amount+1)] for i in coins]
-    
-    # Step 2: Need both index, and coin value
+    for i in range(0, len(coins)):
+        combos[i][0] = 1
+
+    # Step 2: Iterate over coins 
+    #         Need both index, and coin value
+
     for i,coin in enumerate(coins):
 
-        # Step 3: Iterate through column values
+        # Step 3: Iterate through values
         for j in range(0,amount+1):
 
-            # initialize '0' value slot
-            if j == 0:
-                print((i,j))
-                combos[i][j] = 1  
-
+            # Step 4: # combos is:
+            #               whatever you could have done, with just prevoiusly allowed coins
+            #               plus combos of this val - this denom (since this that val, plus this denom)
+            #
             # if current amount > coin val
             # if no previous row, just use a 0 
-            elif j >= coins[i]:
+
+            if j >= coins[i]:
                 if i == 0:
                     combos[i][j] =        0        + combos[i][j-coins[i]]
                 else:
                     combos[i][j] =  combos[i-1][j] + combos[i][j-coins[i]]
                     
 
+            # Step 4: This coin too big? Same as row before, when this coin wasn't allowed
             # if current amount < coin val
             else:
                 combos[i][j] = combos[i-1][j]
 
+
+
     for row in combos:
         print(row)
+
+    # Step 5: return
     return combos
 
 
